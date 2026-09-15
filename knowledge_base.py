7a7c81d96c6f12770331ae7740916ab3806a20aa
abhinav <abhinavspillai7@gmail.com>
2026-09-15 19:52:59 +0530

    intialize clue ai project

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,5 +1,12 @@
-facts=[]
-possible_owners={}
-constrains={}
-conclusions={}
-reasons={}
+class knowledge_base:
+    def __init__(self):
+        self.facts=[]
+        self.possible_owners={}
+        self.constraints={}
+        self.conclusions={}
+        self.reasons={}
+    def add_fact(self,facts):
+        self.facts.append(facts)
+kb = knowledge_base()
+kb.add_fact("Test fact")
+print(kb.facts)
