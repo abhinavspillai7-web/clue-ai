7bfd1e1f68176b4956362406c1c67b518a4dff54
abhinav <abhinavspillai7@gmail.com>
2026-09-19 15:18:01 +0530

    clue-ai v1

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class knowledge_base:
     def __init__(self):
         self.facts=[]
@@ -8,13 +9,17 @@ class knowledge_base:
     def add_fact(self,facts):
         self.facts.append(facts)
 class Fact :
-    def __init__(self,fact_id,suggestor,responder,suggested_cards,responder):
+    def __init__(self,fact_id,suggestor,responder,suggested_cards,response,shown_card,timestamp):
         self.fact_id=fact_id
         self.suggestor=suggestor
         self.responder=responder
         self.suggested_cards=suggested_cards
-        self.responder=responder
+        self.response=response
+        self.shown_card=shown_card
+        self.timestamp=timestamp
+    def __repr__(self):
+        return f"fact id:{self.fact_id},suggestor:{self.suggestor},reponder:{self.responder}"
 kb = knowledge_base()
-fact1=Fact(1,"alice","bob",{"alice","bob","charlie"},"could_not_disprove")        
+fact1=Fact(1,"alice","bob",{"alice","bob","charlie"},"could_not_disprove",None,datetime.now())        
 kb.add_fact(fact1)
 print(kb.facts)
