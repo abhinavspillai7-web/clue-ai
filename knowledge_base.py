a5c016a2a4eef2c913dc8d855b19616e42305073
abhinav <abhinavspillai7@gmail.com>
2026-09-19 15:04:04 +0530

    clue-ai v1

--- knowledge_base.py
+++ knowledge_base.py
@@ -7,6 +7,14 @@ class knowledge_base:
         self.reasons={}
     def add_fact(self,facts):
         self.facts.append(facts)
+class Fact :
+    def __init__(self,fact_id,suggestor,responder,suggested_cards,responder):
+        self.fact_id=fact_id
+        self.suggestor=suggestor
+        self.responder=responder
+        self.suggested_cards=suggested_cards
+        self.responder=responder
 kb = knowledge_base()
-kb.add_fact("Test fact")
+fact1=Fact(1,"alice","bob",{"alice","bob","charlie"},"could_not_disprove")        
+kb.add_fact(fact1)
 print(kb.facts)
